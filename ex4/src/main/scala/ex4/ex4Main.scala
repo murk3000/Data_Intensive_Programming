bab30c55e839edb904fd26e3f23a6c092b70fd51
Muneeb Khan <tbmukh@linux-desktop5.tuni.fi>
2022-11-15 19:38:30 +0200

    Completed Ex4

--- ex4/src/main/scala/ex4/ex4Main.scala
+++ ex4/src/main/scala/ex4/ex4Main.scala
@@ -1,32 +1,27 @@
 package ex4
 
 import org.apache.spark.SparkConf
-
 import org.apache.spark.ml.feature.VectorAssembler
 import org.apache.spark.ml.regression.LinearRegression
 import org.apache.spark.ml.regression.LinearRegressionModel
-
 import org.apache.spark.rdd.RDD
-
 import org.apache.spark.sql.Column
 import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.Dataset
 import org.apache.spark.sql.Row
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.SQLContext
-
 import org.apache.spark.sql.types.StructType
-import org.apache.spark.sql.types.{ArrayType, StringType, StructField, IntegerType, DoubleType}
+import org.apache.spark.sql.types.{ArrayType, DoubleType, IntegerType, StringType, StructField}
 import org.apache.spark.sql.types.DataType
 import org.apache.spark.sql.functions.col
-import org.apache.spark.sql.functions.{count, sum, min, max, asc, desc, udf, to_date, avg}
-
+import org.apache.spark.sql.functions.{asc, avg, count, desc, max, min, sum, to_date, udf}
 import org.apache.spark.storage.StorageLevel
 
 import scala.language.postfixOps
-
 import org.apache.log4j.Logger
 import org.apache.log4j.Level
+import org.apache.spark.ml.Pipeline
 
 
 object Ex4Main extends App {
@@ -63,7 +58,11 @@ object Ex4Main extends App {
   printTaskLine(1)
   // Task 1: Transform dataRDD to a DataFrame dataDF, with two columns "X" (of type Double) and "label" (of type Double).
   //         (The default dependent variable name is "label" in MLlib)
-  val dataDF: DataFrame = ???
+  val df_schema = new StructType(Array(
+    new StructField("X", DoubleType, false),
+    new StructField("label", DoubleType, false)
+  ))
+  val dataDF: DataFrame = spark.createDataFrame(dataRDD, df_schema)
 
 
   // Let's split the data into training and testing datasets
@@ -76,41 +75,55 @@ object Ex4Main extends App {
   printTaskLine(2)
   // Task 2: Create a VectorAssembler for mapping input column "X" to "features" column and
   //         apply it to trainingDF in order to create assembled training data frame
-  val vectorAssembler: VectorAssembler = ???
+  val vectorAssembler: VectorAssembler = new VectorAssembler().setInputCols(Array("X")).setOutputCol("features")
 
-  val assembledTrainingDF: DataFrame = ???
+  val data_pipeline = new Pipeline().setStages(Array(
+    vectorAssembler
+  ))
+  val transformer = data_pipeline.fit(trainingDF)
+
+  val assembledTrainingDF: DataFrame = transformer.transform(trainingDF)
   assembledTrainingDF.show()
 
 
 
   printTaskLine(3)
   // Task 3: Create a LinearRegression object and fit using the training data to get a LinearRegressionModel object
-  val lr: LinearRegression = ???
+  val lr: LinearRegression = new LinearRegression()
+    .setMaxIter(10000)
+    .setRegParam(0.01)
+
 
   println(lr.explainParams())
 
-  val lrModel: LinearRegressionModel = ???
+  val lrModel: LinearRegressionModel = lr.fit(assembledTrainingDF)
   lrModel.summary.predictions.show()
 
 
 
   printTaskLine(4)
   // Task 4: Apply the model to the whole dataDF
-  val allPredictions: DataFrame = ???
+  val allPredictions: DataFrame = lrModel.transform(transformer.transform(dataDF))
   allPredictions.show()
 
 
 
   printTaskLine(5)
   // Task 5: Use the LinearRegressionModel to predict y for values [-0.5, 3.14, 7.5]
-  ???
+  import spark.implicits._
+
+  lrModel.transform(transformer.transform(Seq(-0.5, 3.14, 7.5).toDF("X"))).show()
 
 
 
   printTaskLine(6)
   // Task 6: File "data/numbers.csv" contains one column "X" with several more x values.
   //         Use the LinearRegressionModel to predict the corresponding y values for them.
-  val numberPredictionsDF: DataFrame = ???
+  val df_csv = spark.read.options(Map("header"->"true"))
+    .schema(StructType(Array(StructField("X", DoubleType, false))))
+    .csv("data/numbers.csv")
+
+  val numberPredictionsDF: DataFrame = lrModel.transform(data_pipeline.fit(df_csv).transform(df_csv))
   numberPredictionsDF.show()
 
 
@@ -118,7 +131,7 @@ object Ex4Main extends App {
   printTaskLine(7)
   // Task 7: Store the resulting DataFrame from task 6 into the folder "results" in CSV format.
   //         NOTE: It is ok if you get multiple files with long file names
-  ???
+  numberPredictionsDF.select("X", "prediction").write.options(Map("header"->"true")).csv("results/")
 
 
 
